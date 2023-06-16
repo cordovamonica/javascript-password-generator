91723dc90c8e7fc7576698c4cab6afd8ac30cb06
Mona Lisa <cordovamonica.a@gmail.com>
2023-06-15 23:24:16 -0500

    added window prompts for character case and special char.s

--- script.js
+++ script.js
@@ -13,7 +13,14 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~"]
 
+var generatePassword = function() {
+  var upperCaseChar = window.prompt("Click OK to include uppercase characters");
+  var lowerCaseChar = window.prompt("Click OK to include lower case characters");
+  var specialChar = window.prompt("Enter a special character if wanted");
+  
+}
